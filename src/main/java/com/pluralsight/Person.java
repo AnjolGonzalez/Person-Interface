5c19abaddc0c29a1a8705700fe68622c97e03a1c
Angel Gonzalez <agonzalez000@appdev.yearup.org>
2023-11-14 13:32:00 -0500

    Final Commit

--- src/main/java/com/pluralsight/Person.java
+++ src/main/java/com/pluralsight/Person.java
@@ -1,2 +1,17 @@
-package com.pluralsight;public class Person {
+package com.pluralsight;
+
+public class Person implements Comparable<Person> {
+    public String firstName, lastName;
+    public int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Person otherPerson) {
+        return Integer.compare(this.age, otherPerson.age);
+    }
 }
